b4ec31cd6798d556ff447cb957290fcc70f06d19
sami@p.in <sami@p.com>
2023-12-29 20:38:07 +0530

    new commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import "../App.css";
 
 export default function Navbar({ title, aboutText, mode, toggleMode }) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${mode}`}>
       <div className="container-fluid">
-        <a className={`navbar-brand text-${mode === 'light' ? "dark" : "light"}`} href="#">{title}</a>
+        <Link className={`navbar-brand text-${mode === 'light' ? "dark" : "light"}`} to="/">{title}</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style={{color:mode==="dark"?"white":"white",backgroundColor:mode==="dark"?"white":"white"}}>
           <span className="navbar-toggler-icon" ></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className={`nav-link active text-${mode === 'light' ? "dark" : "light"}`} href="#">Home</a>
+              <Link className={`nav-link active text-${mode === 'light' ? "dark" : "light"}`} to="/">Home</Link>
             </li>
             <li className="nav-item">
-              {/* <a className={`nav-link text-${mode === 'light' ? "dark" : "light"}`} href="/about">{aboutText}</a> */}
+              <Link className={`nav-link text-${mode === 'light' ? "dark" : "light"}`} to="/about">{aboutText}</Link>
             </li>
           </ul>
           <div className={`form-check form-switch text-${mode === 'light' ? "dark" : "light"}`}>
